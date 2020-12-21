491112c27a0c269d43c858fdc0bb99c347713bb9
Bruno Rafael <bruninho_lito@hotmail.com>
2020-12-21 19:06:10 -0300

    feat: improve sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import { ReactComponent as HomeIcon } from "../../assets/home.svg";
 
 const Container = styled.div`
-  height: 94vh;
-  left: 0;
+  height: 100%;
   background-color: #e5e5e5;
   border-right: 1px solid #ebeff2;
   min-width: 236px;
+  position: fixed;
+`;
+
+const Logo = styled.div`
+  padding-left: 16px;
+  font-family: "Open Sans";
+  font-size: 24px;
+  font-weight: bold;
+  margin: 20px 0;
 `;
 
 const NavItems = styled.div`
@@ -20,7 +29,7 @@ const NavItem = styled.div`
   cursor: pointer;
   border-radius: 4px;
   background-color: #5cb5fe;
-  padding-left: 5px;
+  padding-left: 10px;
 `;
 
 const NavItemText = styled.span`
@@ -29,15 +38,18 @@ const NavItemText = styled.span`
   font-size: 14px;
   letter-spacing: 0;
   line-height: 24px;
-  font-weight: bold; 
+  font-weight: bold;
+  margin-left: 10px;
 `;
 
 const Sidebar = () => {
   return (
     <Container>
+      <Logo>Loans Control</Logo>
       <NavItems>
         <NavItem>
-          <NavItemText>Home</NavItemText>
+          <HomeIcon height={20} width={20} />
+          <NavItemText> Home</NavItemText>
         </NavItem>
       </NavItems>
     </Container>
